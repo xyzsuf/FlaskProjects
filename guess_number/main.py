9cb13f46d6659b7f828f1f1f27490ca356febca0
xyzsuf <yusuf_in@outlook.com>
2023-03-25 22:48:47 +0530

    initial commit

---
+++ guess_number/main.py
@@ -0,0 +1,18 @@
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def hello_world():
+    return "<h1> Guess a number between 0 and 9 </h1>" "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif' />"
+
+
+if __name__ == ("__main__"):
+    app.run(port=8082, debug=True)
+
+ 
+
